da38ecf90781268104f3ded19dc0371fec5a67f2
Morten Høfft <mhoefft@gbif.org>
2022-02-03 15:30:30 +0100

    carousel block started

--- app/views/pages/home/home.entry.js
+++ app/views/pages/home/home.entry.js
@@ -64,6 +64,66 @@ function homeCtrl($http, suggestEndpoints, Page) {
     vm.updateSearch = function() {
         location.href = '/search?q=' + encodeURIComponent(vm.freeTextQuery);
     };
+
+
+
+
+
+    vm.myInterval = 0;
+    vm.noWrapSlides = false;
+    vm.active = 0;
+    var slides = vm.slides = []; 
+    var currIndex = 0;
+
+  vm.addSlide = function() {
+    var newWidth = 600 + slides.length + 1;
+    slides.push({
+      image: '//unsplash.it/' + newWidth + '/300',
+      text: ['Nice image', 'Awesome photograph', 'That is so cool', 'I love that'][slides.length % 4],
+      id: currIndex++
+    });
+  };
+
+  vm.randomize = function() {
+    var indexes = generateIndexesArray();
+    assignNewIndexesToSlides(indexes);
+  };
+
+  for (var i = 0; i < 4; i++) {
+    vm.addSlide();
+  }
+
+  // Randomize logic below
+
+  function assignNewIndexesToSlides(indexes) {
+    for (var i = 0, l = slides.length; i < l; i++) {
+      slides[i].id = indexes.pop();
+    }
+  }
+
+  function generateIndexesArray() {
+    var indexes = [];
+    for (var i = 0; i < currIndex; ++i) {
+      indexes[i] = i;
+    }
+    return shuffle(indexes);
+  }
+
+  // http://stackoverflow.com/questions/962802#962890
+  function shuffle(array) {
+    var tmp, current, top = array.length;
+
+    if (top) {
+      while (--top) {
+        current = Math.floor(Math.random() * (top + 1));
+        tmp = array[current];
+        array[current] = array[top];
+        array[top] = tmp;
+      }
+    }
+
+    return array;
+  }
 }
 
 module.exports = homeCtrl;
